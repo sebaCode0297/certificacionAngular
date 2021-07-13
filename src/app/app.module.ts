f98e379df1842ca187fd729264dc13f598c30758
sebaCode0297 <andreecamilo10@gmail.com>
2021-07-12 22:00:11 -0400

    Inicio de certificacion en Universidad - Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonasComponent } from './personaTest/persona.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    PersonasComponent,
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
@@ -15,4 +18,4 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class  AppModule { }
---
+++ src/app/personaTest/persona.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-personas',
+  templateUrl: './personas.component.html',
+  styleUrls: [],
+})
+export class PersonasComponent{
+  titulo2 = '***Saludos desde component Personas***';
+}
